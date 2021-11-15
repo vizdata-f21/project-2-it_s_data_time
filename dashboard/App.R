f0cacee464cb3571c3b6730959023c546bb93555
Raffay Rana <ar534@duke.com>
2021-11-15 09:56:38 -0800

    loaded dataset and started rating tab

--- dashboard/App.R
+++ dashboard/App.R
@@ -5,7 +5,12 @@ library(tidyverse)
 # Load data ---------------------------------------------------------
 
 ratings <- read_csv("../data/IMDbratings.csv")
-movies <- read_csv("../data/IMDb movies.csv")
+
+movies <- read_csv("../data/IMDb movies.csv") %>%
+  filter(year >= 1950)
+
+movies_ratings <- left_join(movies, ratings, by = "imdb_title_id")
+
 title_principles <- read_csv("../data/IMDb title_principals.csv")
 
 # Shiny UI
@@ -13,17 +18,28 @@ ui <- navbarPage(inverse = TRUE, "Analysis of Movies",
                  #Page 1.
 
                  tabPanel("Gengres"),
-                 tabPanel("Raffay Tab"),
+                 tabPanel(
+                   title = "Raffay Tab",
+                   DT::dataTableOutput(outputId = "datatb")
+                   ),
                  tabPanel("Davis Tab"),
                  tabPanel("Martha Tab")
-
-
 )
 
 
 # SHINY SERVER
 
- server <- function(input, output) {}
+ server <- function(input, output,session) {
+
+   movie_dt <- reactive(movies_ratings)
+
+   output$datatb<- DT::renderDataTable({
+     # Remind Shiny it is a reactive objective
+     # DO NOT FORGET THIS
+     movie_dt()
+   })
+
+ }
 
 
 
