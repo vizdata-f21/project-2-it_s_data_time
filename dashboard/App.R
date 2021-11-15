f81cb2b39a429585b4e05fc71aa42b3641098544
Raffay Rana <ar534@duke.com>
2021-11-15 09:31:05 -0800

    Loaded data to Shiny App

--- dashboard/App.R
+++ dashboard/App.R
@@ -1,8 +1,14 @@
+# Load packages -----------------------------------------------------
 library(shiny)
+library(tidyverse)
+
+# Load data ---------------------------------------------------------
+
+ratings <- read_csv("../data/IMDbratings.csv")
+movies <- read_csv("../data/IMDb movies.csv")
+title_principles <- read_csv("../data/IMDb title_principals.csv")
 
 # Shiny UI
-
-
 ui <- navbarPage(inverse = TRUE, "Analysis of Movies",
                  #Page 1.
 
