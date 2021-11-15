b36ff104f4df3d96a109cbfbb76f9fd9a37c5e68
AbMartha <mfa23@duke.edu>
2021-11-15 12:15:40 -0500

    set up tabs

--- dashboard/App.R
+++ dashboard/App.R
@@ -3,15 +3,24 @@ library(shiny)
 # Shiny UI
 
 
-##ui <-
+ui <- navbarPage(inverse = TRUE, "Analysis of Movies",
+                 #Page 1.
+
+                 tabPanel("Gengres"),
+                 tabPanel("Raffay Tab"),
+                 tabPanel("Davis Tab"),
+                 tabPanel("Martha Tab")
+
+
+)
 
 
 # SHINY SERVER
 
-## server <- function(input, output) {}
+ server <- function(input, output) {}
 
 
 
 # Run application.
 
-#shinyApp(ui = ui, server = server)
+shinyApp(ui = ui, server = server)
