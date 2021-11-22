5f23437c69df98c9d59622cfb3c9029779a9d191
AbMartha <mfa23@duke.edu>
2021-11-22 12:37:33 -0500

    adding hyperlegible font

--- dashboard/App.R
+++ dashboard/App.R
@@ -12,7 +12,7 @@ title_principles <- read_csv("../data/IMDb title_principals.csv")
 # Shiny UI
 ui <- navbarPage(inverse = TRUE, "Analysis of Movies",
                  #Page 1.
-                 tabPanel("Genres or Krystals Tab",  includeCSS("styles.css")),
+                 tabPanel("Genres or Krystals Tab",  includeCSS("css/styles.css")),
                  tabPanel("Scores Tab"),
                  tabPanel("Peoples tab.",
                           fluidPage(h1("Analysis of People involved in the film making. ")
